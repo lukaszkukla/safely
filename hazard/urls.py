122f0b3b38160d8ce1ea5844f4e97867cd0166b6
lukaszkukla <lukaszkukla@gmail.com>
2022-07-31 12:03:14 +0000

    Feat: add hazard detailed view

--- hazard/urls.py
+++ hazard/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import HazardList
+from .views import HazardList, HazardDetail
 
 
 urlpatterns = [
     path('', HazardList.as_view(), name='hazards'),
+    path('hazard/<int:pk>/', HazardDetail.as_view(), name='hazard'),
 ]
--- hazard/views.py
+++ hazard/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from .models import Hazard
 
 
 class HazardList(ListView):
     model = Hazard
+    context_object_name = 'hazards'
+
+class HazardDetail(DetailView):
+    model = Hazard
+    context_object_name = 'hazard'
+    template_name = 'hazard/hazard.html'
