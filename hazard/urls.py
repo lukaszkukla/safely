fb2fa552ded08e48e956933fdd994eb122a5c5ed
lukaszkukla <lukaszkukla@gmail.com>
2022-08-01 21:52:36 +0000

    Feat: add Login/Logout functionality

--- hazard/urls.py
+++ hazard/urls.py
@@ -1,8 +1,18 @@
 from django.urls import path
-from .views import HazardList, HazardDetail, HazardCreate, HazardUpdate, HazardDelete
+from .views import (
+    HazardList,
+    HazardDetail,
+    HazardCreate,
+    HazardUpdate,
+    HazardDelete,
+    CustomLoginView
+)
+from django.contrib.auth.views import LogoutView
 
 
 urlpatterns = [
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(next_page='hazards'), name='logout'),
     path('', HazardList.as_view(), name='hazards'),
     path('hazard/<int:pk>/', HazardDetail.as_view(), name='hazard'),
     path('hazard-create/', HazardCreate.as_view(), name='hazard-create'),
