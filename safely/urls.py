d21032f4fca567fadcba413be13e5e38d8f7e1f7
lukaszkukla <lukaszkukla@gmail.com>
2022-08-28 03:12:29 +0000

    Change debug mode to production

--- safely/urls.py
+++ safely/urls.py
@@ -17,11 +17,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.contrib.staticfiles.urls import static
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('hazard.urls')),
     path('', include('django.contrib.auth.urls')),
+    path(r'^static/(?P<path>.*)$', serve,
+         {'document_root': settings.STATIC_ROOT}),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 handler404 = 'hazard.views.error404View'
 handler500 = 'hazard.views.server_error_500'
